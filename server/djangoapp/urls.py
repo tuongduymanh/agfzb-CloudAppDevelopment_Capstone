dc222bd1542b98fcfff893c9005ffb7ac6fa441c
tuongduymanh <tuongduymanh9x@gmail.com>
2021-09-27 02:23:35 +0700

    manhtd commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,18 +10,22 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route = 'about/', view = views.about, name='about'),
 
     # path for contact us view
+    path(route = 'contact/', view = views.contact, name='contact'),
 
     # path for registration
-
+    path('signup/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
+    path('logout/', views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
+    path('dealer/<int:dealer_id>/', views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
 
--- server/djangobackend/settings.py
+++ server/djangobackend/settings.py
@@ -25,9 +25,7 @@ SECRET_KEY = 'ao5z(o(z@cvzodm99d32jkxa5e8a1!q_4sqss5-a%n6tg$#h$+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-APPEND_SLASH = True
-
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = ["devaina21.us-south.cf.appdomain.cloud","localhost"]
 
 
 # Application definition
