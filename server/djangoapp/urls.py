31086e0643da74f829f80c7e80241412d3b4542f
Admin <Admin@DESKTOP-QNP20TN>
2021-09-28 00:52:28 +0700

    manhtd commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from ..djangobackend import views
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 from django.contrib import admin
 
 app_name = 'djangoapp'
